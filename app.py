0093448390434df9f2837cfeb3dced4c6c62a285
M4NUK4N0 <manuela.canoc5@gmail.com>
2026-02-26 10:03:30 -0500

    Add image and headers to Streamlit app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Esta es mi primera app en la nube")
+
+st.header("Mi primera App :)")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open("Musiquita.gif")
+st.image(image,caption = "Interfaces Multimodales")
