3aafdf04dacc9e54cda770cb3e97e60e311208b4
M4NUK4N0 <manuela.canoc5@gmail.com>
2026-02-26 10:24:14 -0500

    Implement two-column layout with checkbox in app.py
    Added two-column layout with checkbox interaction.

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ st.image(image,caption = "Interfaces Multimodales")
 
 texto = st.text_input("Escribe algo" , "Este es mi texto")
 st.write("El texto escrito es" , texto)
+
+st.suheader("Ahora usemos dos columnas columnas")
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia del usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("Correcto! :D")
